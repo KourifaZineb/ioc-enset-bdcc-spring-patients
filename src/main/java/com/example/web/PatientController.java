2ae54ac1b20bd6a53132dddc192f0d2099f1028c
KourifaZineb <zinebkourifa@gmail.com>
2024-03-15 20:54:30 +0000

    pagination & database

--- src/main/java/com/example/web/PatientController.java
+++ src/main/java/com/example/web/PatientController.java
@@ -3,6 +3,8 @@ package com.example.web;
 import com.example.entities.Patient;
 import com.example.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,16 @@ public class PatientController {
     }*/
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "5") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword){
+        //Pour faire la pagination
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        //List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
         return "Patients";
     }
 
